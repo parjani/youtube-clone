47bc2b30747ff50b972ec9d22a589371f440d5ae
parjani <sangvikarparjani111@gmail.com>
2026-05-17 09:08:52 +0530

    Updated new field in video

--- server/Models/Video.Model.js
+++ server/Models/Video.Model.js
@@ -1,18 +1,62 @@
 import mongoose from "mongoose";
 
+const commentSchema = new mongoose.Schema(
+  {
+    commentId: String,
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    text: String,
+    timestamp: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { _id: false }
+);
+
 const videoSchema = new mongoose.Schema(
   {
+    videoId: String,
+
     title: String,
     description: String,
+
     thumbnailUrl: String,
     videoUrl: String,
+
     category: String,
-    views: Number,
-    likes: Number,
-    dislikes: Number,
+
+    channel: String,
+
+    channelId: String,
+
     uploader: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
+    },
+
+    views: {
+      type: Number,
+      default: 0,
+    },
+
+    likes: {
+      type: Number,
+      default: 0,
+    },
+
+    dislikes: {
+      type: Number,
+      default: 0,
+    },
+
+    comments: [commentSchema],
+
+    uploadDate: {
+      type: Date,
+      default: Date.now,
     },
   },
   { timestamps: true }
