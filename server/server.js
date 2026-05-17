4d0d83a0b7558706a341d036ba1f2a5cc46caa24
parjani <sangvikarparjani111@gmail.com>
2026-05-17 09:10:06 +0530

    Uodated the path of new route and modified some existing

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ app.use(express.json());
 // Routes
 app.use("/api/auth", authRoutes);
 
-app.use("/api/channels", channelRoutes);
+app.use("/api/channel", channelRoutes);
 
 app.use("/api/comments", commentRoutes);
 
